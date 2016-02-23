d023c0caa9edb5824066b1354c4e8f0adae204ed
Ryan H. Banta <rbanta027@gmail.com>
2016-02-23 19:19:10 +0800

    changed function name MyFunc to FuncOut

---
+++ function_stdoutput.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+def FuncOut(name,age):
+    print "Hi! My name is %s and my age is %d " % (name,age)
+    print "Hi! My name is " +name+ " and my age is", age
+    print "Hi! My name is", name , "and my age is", age
+
+
+FuncOut('Mary',19)
